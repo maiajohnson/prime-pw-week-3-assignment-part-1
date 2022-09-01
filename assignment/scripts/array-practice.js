b1ee473810186c47539d19e57fe42419d27abd5f
Maia Johnson <maiaj1306@gmail.com>
2022-09-01 11:08:42 -0500

    added and removed array items from beginning of arrays

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -68,7 +68,8 @@ animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+foodArray.unshift('apple');
+console.log(`Added a food to beginning: ${foodArray}`);
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
@@ -77,6 +78,9 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array & 
 //     log both the food removed and the updated array
+removedFood = foodArray.shift();
+console.log('Removed the first food', removedFood);
+console.log('The foods are now', foodArray);
 
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
